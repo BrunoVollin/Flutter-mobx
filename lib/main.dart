3c38ae87f52ad36f4498822d29ca803b4fdf8328
Bruno <bruno.3av@gmail.com>
2021-11-16 17:06:09 -0300

    init

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flut_mobx/controller.dart';
+import 'package:flutter/material.dart';
+import "package:flutter_mobx/flutter_mobx.dart";
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final controller = Controller();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("widget.title"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Observer(builder: (_) {
+              return Text(
+                '${controller.counter}',
+                style: Theme.of(context).textTheme.headline4,
+              );
+            }),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          controller.increment!();
+          },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
